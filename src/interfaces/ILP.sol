9216af9edcc0c4e8c2857ae87b2bf2edd0b3a2cc
luciferzxj <106872309+luciferzxj@users.noreply.github.com>
2024-02-18 11:14:04 +0800

    Delete src directory

--- src/interfaces/ILP.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12;
-
-import '@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol';
-
-interface ILP is IERC20Metadata {
-     function mint(address to, uint256 amount) external; 
-     function burn(address from, uint256 amount) external;
-}
