8a9293e1e10be799abcb89f5fcf4285b4e4927b7
luciferzxj <106872309+luciferzxj@users.noreply.github.com>
2024-03-08 00:07:34 +0800

    Add files via upload

--- src/interfaces/ISwapRouter.sol
+++ src/interfaces/ISwapRouter.sol
@@ -2,19 +2,26 @@
 pragma solidity ^0.8.12;
 
 interface ISwapRouter {
-    struct RouterConfig {
-        address uniswapV3Router;
-        address uniswapV2Router;
-    }
+    // struct RouterConfig {
+    //     address router;
+    //     Type ty;
+    // }
+    // enum Type {
+    //     UNIV3,
+    //     UNIV2,
+    //     OTHER
+    // }
 
     struct UniV3Data {
         bytes path;
         uint256 ratio;
+        uint256 index;
     }
 
     struct UniV2Data {
         address[] path;
         uint256 ratio;
+        uint256 index;
     }
 
     struct Strategy {
@@ -25,17 +32,41 @@ interface ISwapRouter {
 
     function getSlippage() external view returns (uint256 slippage);
 
-    function buy(
-        address spendAsset,
-        uint256 buyAmount,
-        address buyAsset,
-        uint256 spendAssetMax
-    ) external returns (uint256 spendAmount);
+    function swapCustomIn(
+        address tokenIn,
+        uint256 amountInMax,
+        address tokenOut,
+        uint256 amountOut
+    ) external returns (uint256 amountIn);
 
-    function sell(
-        address sellAseet,
-        uint256 sellAseetAmount,
-        address returnAsset,
-        uint256 returnAssetMin
-    ) external returns (uint256 returnAmount);
+    function swapCustomOut(
+         address tokenIn,
+         uint256 amountIn,
+         address tokenOut,
+         uint256 amountOutMin
+    ) external returns (uint256 amountOut);
+
+    function updateSlippage(uint256 _newSlippage) external;
+
+    function addUniV3Strategy(address tokenIn, address tokenOut, UniV3Data memory datas) external;
+
+    function addUniV2Strategy(address tokenIn, address tokenOut, UniV2Data memory datas) external;
+
+    function addRouter(address _newRouter)external;
+
+    function deleteRouter(uint256 index)external;
+    
+    function updateUniV3Strategy(
+        address tokenIn,
+        address tokenOut,
+        UniV3Data memory datas,
+        uint256 index
+    ) external;
+
+    function updateUniV2Strategy(
+        address tokenIn,
+        address tokenOut,
+        UniV2Data memory datas,
+        uint256 index
+    ) external;
 }
